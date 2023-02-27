ab2249b1908d772244a837c9c2f5b3c917651b3d
azultasul <azultasul@gmail.com>
2023-02-27 23:11:37 +0900

    scss 세팅

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   webpack: config => {
@@ -9,7 +10,10 @@ const nextConfig = {
       }
     );
     return config
-  }
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
